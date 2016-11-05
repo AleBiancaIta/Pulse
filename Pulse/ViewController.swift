c82e78725747e397d7f3f34c9356b01d7b6d5452
Bianca Curutan <bmcurutan@gmail.com>
2016-11-04 20:47:40 -0700

    Added empty project

---
+++ Pulse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pulse
+//
+//  Created by Bianca Curutan on 11/4/16.
+//  Copyright © 2016 ABI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
