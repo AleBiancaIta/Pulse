171786f036fd8644ed3d716f1b43a948ac21be4c
anegrete <ale@Alejandras-MacBook-Pro.local>
2016-11-09 01:11:19 -0800

    Parse initialization

--- Pulse/AppDelegate.swift
+++ Pulse/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+
+		Parse.initialize(with:
+			ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+				configuration.applicationId = "pulseAleBiancaIta"
+				configuration.clientKey = nil  // set to nil assuming you have not set clientKey
+				configuration.server = "https://pu1se.herokuapp.com/parse"
+			})
+		)
+
+		return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
