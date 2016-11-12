a56dde36083980ad8d631da1f81cd3fbe76b7bf1
itasari <itasari@gmail.com>
2016-11-11 16:30:07 -0800

    Implement sign up logic

--- Pulse/LoginSignUpViewController.swift
+++ Pulse/LoginSignUpViewController.swift
@@ -12,7 +12,6 @@ class LoginSignUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
