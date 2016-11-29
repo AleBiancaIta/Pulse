a1a533d3a16abe8349393ec6e528efceaa26d56e
itasari <itasari@gmail.com>
2016-11-28 17:19:41 -0800

    Enable return key on usernameTextField in Login page

--- Pulse/LoginViewController.swift
+++ Pulse/LoginViewController.swift
@@ -18,6 +18,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        usernameTextField.delegate = self
         passwordTextField.delegate = self
     }
     
